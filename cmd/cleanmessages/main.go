d84d65e133e51f7ae85dc5454f6aba39373095db
Sam Al-Sapti <sam@sapti.me>
2022-12-10 23:19:51 +0100

    Move flag vars out of the main function

--- cmd/cleanmessages/main.go
+++ cmd/cleanmessages/main.go
@@ -9,22 +9,21 @@ import (
 )
 
 var (
-	port    int
-	dirPath string // currently just a JSON file path
+	// currently just a JSON file path
+	path *string = flag.String("path", "", "Path to the directory containing your Facebook data (required)")
+	port *int    = flag.Int("port", 8080, "Port to listen on")
 )
 
 func main() {
-	flag.IntVar(&port, "port", 8080, "Port to listen on")
-	flag.StringVar(&dirPath, "path", "", "Path to the directory containing your Facebook data (required)")
 	flag.Parse()
 
-	if len(dirPath) == 0 {
-		utils.PrintError("You must provide the path to your messages directory")
+	if len(*path) == 0 {
+		utils.PrintError("error: -path must be specified\n")
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	conv, err := conversation.Parse(dirPath)
+	conv, err := conversation.Parse(*path)
 	if err != nil {
 		panic("Error parsing JSON. Aborting...")
 	}
